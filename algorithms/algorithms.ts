11ddda2267419192f05e8bf9c97c9977adca3b2f
caciano almeida <oalme004@fiu.edu>
2021-03-27 00:17:33 -0400

    completed A* algorithm

--- algorithms/algorithms.ts
+++ algorithms/algorithms.ts
@@ -2,22 +2,22 @@
 
 import { AdjacencyList, Weights } from './utils';
 
-// TODO implement dijkstra's, BFS, DFS, A*, bellman-ford, bidirectional
-// TODO optional best-first-search, D*
+// TODO implement BFS, DFS, bellman-ford, bidirectional
+// TODO optional best-first-search, D* or D* lite, floyd-warshell, johnsons
 
-export type Paths = Map<number, { length: number; path: number[] }>;
-export type Explored = { paths: Paths; visited: number[] };
-type Node = { position: number; value: number; path: number[] };
+type SinglePath = { length: number; path: number[] };
+export type Path = Map<number, SinglePath> | SinglePath;
+export type Explored = { paths: Path; visited: number[] };
+type AstarNode = { key: number; movement: number; value: number; path: number[] };
 
-export const dijkstra = (adjacencyList: AdjacencyList, start: number, weights: Weights = new Map()): Explored => {
-  const paths: Paths = new Map();
-  for (const key of adjacencyList.keys()) paths.set(key, { length: Infinity, path: [] });
+export const dijkstra = (list: AdjacencyList, start: number, weights: Weights = new Map()): Explored => {
+  const paths: Path = new Map();
+  for (const key of list.keys()) paths.set(key, { length: Infinity, path: [] });
   paths.set(start, { length: 0, path: [start] });
   const visited: number[] = [];
-
-  const fn = (current = start): Paths => {
+  const fn = (current = start): Path => {
     visited.push(current);
-    const neighbors = adjacencyList.get(current)!;
+    const neighbors = list.get(current)!;
     const currentPath = paths.get(current)!;
     for (const neighbor of neighbors) {
       const neighborPath = paths.get(neighbor)!;
@@ -27,29 +27,79 @@ export const dijkstra = (adjacencyList: AdjacencyList, start: number, weights: W
     for (const neighbor of neighbors) if (!visited.includes(neighbor)) fn(neighbor);
     return paths;
   };
-
   return { paths: fn(), visited };
 };
 
-export const Astar = (
-  adjacencyList: AdjacencyList,
-  start: number,
-  destination: number,
-  rowLength: number,
-  weights: Weights = new Map(),
-): Explored => {
-  const paths: Paths = new Map();
-  const closed: Map<number, Node> = new Map();
-  const open: Map<number, Node> = new Map();
-
-  return { paths, visited: [...open.keys()] };
+export const Astar = (list: AdjacencyList, start: number, dest: number, row: number, weights?: Weights): Explored => {
+  const startNode = { key: start, movement: 0, value: heuristic(start, dest, row), path: [start] };
+  const open = new PriorityQueue(startNode);
+  const closed: Map<number, AstarNode> = new Map();
+  const fn = (): Path => {
+    const node = open.deQueue()!;
+    closed.set(node.key, node);
+    if (node.key === dest) return { length: node.movement, path: node.path };
+    for (const adjacent of list.get(node.key)!) {
+      const weight = weights?.get(adjacent)?.weight || 1;
+      const value = heuristic(adjacent, dest, row) + weight;
+      const notInLists = !open.get(adjacent) && !closed.get(adjacent);
+      const betterThanClosed = closed.get(adjacent) && value < closed.get(adjacent)!.value;
+      const betterThanOpen = open.get(adjacent) && value < open.get(adjacent)!.value;
+      if (notInLists || betterThanClosed || betterThanOpen)
+        open.set({ key: adjacent, movement: node.movement + weight, value, path: [...node.path, adjacent] });
+    }
+    return fn();
+  };
+  return { paths: fn(), visited: open.explored() };
 };
 
-// A* algorithm can use different types of heuristics, this is an implementation of manhattan-distance
-function heuristic(current: number, destination: number, rowLength: number): number {
-  const yDiff = Math.abs(Math.ceil(current / rowLength) - Math.ceil(destination / rowLength));
-  const xCurrent = current % rowLength === 0 ? rowLength : current % rowLength;
-  const xDestination = destination % rowLength === 0 ? rowLength : destination % rowLength;
+//=========================================Helper Functions=========================================
+
+/*
+    A* algorithm can use different types of heuristics, this is an implementation of manhattan-distance,
+    a heuristic that gaurantees shortest path and works when you have travel in 4 directions(up,left,down,right)
+ */
+function heuristic(current: number, dest: number, row: number): number {
+  const yDiff = Math.abs(Math.ceil(current / row) - Math.ceil(dest / row));
+  const xCurrent = current % row === 0 ? row : current % row;
+  const xDestination = dest % row === 0 ? row : dest % row;
   const xDiff = Math.abs(xCurrent - xDestination);
   return xDiff + yDiff;
+}
+
+/*  
+  Priority Queue used for A* algorithm, useful optimization since 
+  there is the need to constantly retrieving the node with smallest value
+*/
+class PriorityQueue {
+  private Dictionary: Map<number, AstarNode> = new Map();
+  private Queue: AstarNode[] = [];
+  private Explored: number[] = [];
+  constructor(startNode: AstarNode) {
+    this.set(startNode);
+  }
+  deQueue(): AstarNode | undefined {
+    const removed = this.Queue.shift();
+    if (removed) this.Explored.push(removed.key);
+    return removed;
+  }
+  get(key: number): AstarNode | undefined {
+    return this.Dictionary.get(key);
+  }
+  explored(): number[] {
+    return [...this.Explored, ...this.Queue.map(node => node.key)];
+  }
+  set(item: AstarNode): void {
+    let middle = this.Queue.length === 0 ? 0 : Math.floor((this.Queue.length - 1) / 2);
+    for (let start = 0, end = this.Queue.length - 1; end >= start; middle = Math.floor((end - start) / 2) + start) {
+      if (middle === 0 || item.value === this.Queue[middle].value) break;
+      else if (item.value < this.Queue[middle].value) end = middle - 1;
+      else start = middle + 1;
+    }
+    this.Dictionary.set(item.key, item);
+    if (this.Queue.length === 0) this.Queue.push(item);
+    else {
+      if (this.Queue[middle].value < item.value) middle++;
+      this.Queue.splice(middle, 0, item);
+    }
+  }
 }
